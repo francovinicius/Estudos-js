c55729c303c8211f834a865cae60b7fada3966c2
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2022-12-16 13:15:56 -0300

    setando a hora do relogio digital

--- Projeto 02/script.js
+++ Projeto 02/script.js
@@ -5,3 +5,16 @@ let digitalElement = document.querySelector('.digital');
 let sElement = document.querySelector('.p_s');
 let mElement = document.querySelector('.p_m');
 let hElement = document.querySelector('.p_h');
+
+function updadeClock(){
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+                    //inner pra eu poder colocar um texto
+    digitalElement.innerHTML = `${hour}:${minute}:${second}`;
+}
+
+//intervalo infinito de tempo em tempo rodando em msegundos
+setInterval(updadeClock, 1000);
