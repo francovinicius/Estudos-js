6a5c2a116ee79540ed06c65ce817422e9537a743
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2022-12-16 13:08:58 -0300

    selecionando os elementos

---
+++ Projeto 02/script.js
@@ -0,0 +1,7 @@
+//selecionar os elementos
+
+//relogios 1 digital 2 ponteiros segundos 3 minutos 4 horas
+let digitalElement = document.querySelector('.digital');
+let sElement = document.querySelector('.p_s');
+let mElement = document.querySelector('.p_m');
+let hElement = document.querySelector('.p_h');
