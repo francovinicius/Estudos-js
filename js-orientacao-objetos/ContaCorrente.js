91387f7a685d79524abe3327c39b28d040a98694
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2022-12-30 14:20:41 -0300

    criando construtores

--- js-orientacao-objetos/ContaCorrente.js
+++ js-orientacao-objetos/ContaCorrente.js
@@ -1,34 +1,54 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
-    agencia;
-    cliente;
+  agencia;
+
+  _cliente;
+
+  //# -> torna o campo privada https://github.com/tc39/proposal-class-fields#private-fields
+  // mas usa-se o _ na comunidade para privado
+  _saldo = 0;
 
 
-    //# -> torna o campo privada https://github.com/tc39/proposal-class-fields#private-fields
-    // mas usa-se o _ na comunidade
-    _saldo = 0;
-    
-  
-    sacar(valor) {
-      //this desse saldo (parametro)
-      if (this._saldo >= valor) {
-        this._saldo -= valor;
-  
-        return valor;
-      }
-    }
-  
-    depositar(valor) {
-      //melhor forma de escrever quando tem muitos ifs colocar as condições que não queremos para retornar primeiro
-      if (valor <= 0) {
-        return;
-      }
-      this._saldo += valor;
-    }
 
-    transferir(valor, conta) {
-                                        //
-        const valorSacado = this.sacar(valor);
-        //
-        conta.depositar(valorSacado);
+  /* get e set são assessores*/
+  //atribuir
+  set cliente(novoValor) {
+    if (novoValor instanceof Cliente) {
+      this._cliente = novoValor;
     }
   }
+  //pegar
+  get cliente() {
+    return this._cliente;
+  }
+  get saldo() {
+    return this._saldo;
+  }
+
+
+  
+  sacar(valor) {
+    //this desse saldo (parametro)
+    if (this._saldo >= valor) {
+      this._saldo -= valor;
+
+      return valor;
+    }
+  }
+
+  depositar(valor) {
+    //melhor forma de escrever quando tem muitos ifs colocar as condições que não queremos para retornar primeiro
+    if (valor <= 0) {
+      return;
+    }
+    this._saldo += valor;
+  }
+
+  transferir(valor, conta) {
+    //
+    const valorSacado = this.sacar(valor);
+    //
+    conta.depositar(valorSacado);
+  }
+}
--- js-orientacao-objetos/index.js
+++ js-orientacao-objetos/index.js
@@ -2,27 +2,37 @@ import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
 
-const cliente1 = new Cliente();
+const cliente1 = new Cliente("Ricardo", 11122233309);
 
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
+const cliente2 = new Cliente("Alice", 88822233309);
+
+const contaCorrente1 = new ContaCorrente();
+contaCorrente1.agencia = 1001;
+contaCorrente1.cliente = cliente1;
+contaCorrente1.depositar(500);
+
+const contaCorrente2 = new ContaCorrente();
+contaCorrente2.agencia = 2002;
+contaCorrente2.cliente = cliente2;
+contaCorrente2.depositar(300);
+
+/*
+primeiro modo
 
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
+
+
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
 contaCorrenteRicardo.cliente = cliente1;
-
 contaCorrenteRicardo.depositar(500);
 
 const conta2 = new ContaCorrente();
-conta2.agencia = 1001
 conta2.cliente = cliente2;
+conta2.agencia = 102;
+*/
 
-console.log(contaCorrenteRicardo, conta2);
-
-contaCorrenteRicardo.transferir(150, conta2);
-
-console.log(contaCorrenteRicardo, conta2);
+console.log(contaCorrente1, contaCorrente2)
