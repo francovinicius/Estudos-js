13e37332e5173336c2bb036e5935422d2c8b45c5
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2022-12-30 10:10:51 -0300

    avançando no sacar/depositar

--- js-orientacao-objetos/index.js
+++ js-orientacao-objetos/index.js
@@ -7,6 +7,19 @@ class Cliente{
 class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor) {
+            //this desse saldo (parametro)
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+        }
+    }
+
+    depositar(valor) {
+        if(valor > 0) {
+            this.saldo += valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
@@ -20,18 +33,14 @@ cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.saldo = 0;
+
+contaCorrenteRicardo.saldo = 0
 contaCorrenteRicardo.agencia = 1001;
 
-console.log(contaCorrenteRicardo.saldo);
-contaCorrenteRicardo.saldo = 100;
-console.log(contaCorrenteRicardo.saldo);
 
-let valorSacado = 200;
 
-contaCorrenteRicardo.saldo -= valorSacado;
 
-console.log(contaCorrenteRicardo.saldo)
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.sacar(50);
 
-console.log(cliente1);
-console.log(cliente2);
+console.log(contaCorrenteRicardo);
