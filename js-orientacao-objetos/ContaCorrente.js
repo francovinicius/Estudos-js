d5a8d82e87fa4789360a0431066bef57e85c00dc
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2022-12-30 10:38:02 -0300

    separando as classes

---
+++ js-orientacao-objetos/ContaCorrente.js
@@ -0,0 +1,23 @@
+class ContaCorrente {
+    agencia;
+    //# -> torna o campo privada https://github.com/tc39/proposal-class-fields#private-fields
+    // mas usa-se o _ na comunidade
+    _saldo = 0;
+  
+    sacar(valor) {
+      //this desse saldo (parametro)
+      if (this._saldo >= valor) {
+        this._saldo -= valor;
+  
+        return valor;
+      }
+    }
+  
+    depositar(valor) {
+      //melhor forma de escrever quando tem muitos ifs colocar as condições que não queremos para retornar primeiro
+      if (valor <= 0) {
+        return;
+      }
+      this._saldo += valor;
+    }
+  }
--- js-orientacao-objetos/index.js
+++ js-orientacao-objetos/index.js
@@ -1,34 +1,8 @@
-class Cliente{
-    nome;
-    cpf;
- 
-}
-
-class ContaCorrente {
-    agencia;
-    //# -> torna o campo privada https://github.com/tc39/proposal-class-fields#private-fields
-    // mas usa-se o _ na comunidade
-    _saldo = 0;
-
-    sacar(valor) {
-            //this desse saldo (parametro)
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-        }
-    }
-
-    depositar(valor) {
-        if(valor > 0) {
-            this._saldo += valor;
-        }
-    }
-}
 
 const cliente1 = new Cliente();
 
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-
 
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
@@ -37,10 +11,11 @@ cliente2.cpf = 88822233309;
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
 
-
-
-
 contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.sacar(50);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
 
 console.log(contaCorrenteRicardo);
